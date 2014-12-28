6c244595797ac2d3c6a4490a295ac664bb296ff2
Matthaus <mjwolff@umass.edu>
2014-12-27 19:12:39 -0500

    search history function

--- app/search.js
+++ app/search.js
@@ -6,9 +6,21 @@ var main = function() {
 	var changefunc = function(){
 		document.getElementById("searchResultField").innerHTML= document.getElementById("mainSearchBox").value
 
+		 chrome.history.search({
+		 	"text": document.getElementById("mainSearchBox").value
+		 },
+		 function(historyItems) {
+		 var result = document.createElement("div");
+		 	for(var x = 0; x < historyItems.length; x+=1){
+		 		var temp = document.createElement("P");
+		 		temp.innterHTML = historyItems[x].title;
+		 		result.appendChild(temp);
+		 	}
+		 	document.getElementById("searchResultField").appendChild(result);
+
+		 })
 	}
 	document.getElementById("mainSearchBox").addEventListener("input", changefunc)
-
 }
 
 
