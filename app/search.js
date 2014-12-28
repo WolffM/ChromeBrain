3afb112171f3a2822de3bcbb09038c6e74f4c117
reisepass <rubenwolff@gmail.com>
2014-12-27 19:32:46 -0500

    I dont want to write a commit message MUAHHAHAHAH

--- app/search.js
+++ app/search.js
@@ -4,7 +4,7 @@ var main = function() {
 
 
 	var changefunc = function(){
-		document.getElementById("searchResultField").innerHTML= document.getElementById("mainSearchBox").value
+
 
 		 chrome.history.search({
 		 	"text": document.getElementById("mainSearchBox").value
@@ -13,11 +13,11 @@ var main = function() {
 		 var result = document.createElement("div");
 		 	for(var x = 0; x < historyItems.length; x+=1){
 		 		var temp = document.createElement("P");
-		 		temp.innterHTML = historyItems[x].title;
+		 		temp.innerHTML = historyItems[x].title;
 		 		result.appendChild(temp);
 		 	}
-		 	document.getElementById("searchResultField").appendChild(result);
-
+		 	console.log(result)
+			document.getElementById("searchResultField").innerHTML=result.innerHTML;
 		 })
 	}
 	document.getElementById("mainSearchBox").addEventListener("input", changefunc)
